341054588e2355267fb621c12596978261079c81
guimaroes <gabriel.guimaraes@universo.univates.br>
2022-05-06 00:13:17 -0300

    Create feedback creation rote, saving in sqlite db, sending email

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "1a6cd32cd51746",
+      pass: "5ed6917ab8826f"
+    }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+    async sendMail({ subject, body }: SendMailData) {
+        await transport.sendMail({
+            from: 'Equipe Feedget <oi@feedget.com>',
+            to: 'Gabriel Guimarães <gabriel@email.com>',
+            subject,
+            html: body
+        });
+    }
+}
